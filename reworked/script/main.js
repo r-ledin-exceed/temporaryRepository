f68375dc39507957a6849fc6b4804878147564a0
RomanLedin <ledinroman@yandex.ru>
2021-03-28 02:54:00 +0300

    28.03 upd need fix ondblclick

--- reworked/script/main.js
+++ reworked/script/main.js
@@ -1,17 +1,27 @@
 $(function() {
-    let arr = [],
-    id = 0;
 
+    let arr = [],
+    id = 0,
+    btn = $("#mainMarker"),
+    getter = document.getElementById('task');
+ 
+    
+    // Left Counter
     function itemsLeft() {
         let count = 0;
-        arr.forEach(obj => {     
-            if (!obj.isChecked) { count++ };
+        if (arr.length) {
+            arr.forEach(obj => {     
+                if (!obj.isChecked) { count++ };
+                $("#outputField").text(
+                    `${count} items left`
+                ) 
+            }); 
+        } else {
             $("#outputField").text(
-                `${count} items left`
-            )
-        });
-    }
-
+                `0 items left`
+            ) 
+        };
+    };
 
     // INPUT
     $("#taskInput").keyup(function(event) {    
@@ -20,33 +30,34 @@ $(function() {
             name:$("#taskInput").val(),
             id: id,
             isChecked: false,
+     
         };
 
         if((event.keyCode == 13) && (obj.name != "")) {
             arr.push(obj);
 
             $("#task").append($(`
-            <li id="list${obj.id}"><input type = "text" id="rewrite${obj.id}" class="hidden">
+            <li class="task__listik" id="list${obj.id}"><input type = "text" id="rewrite${obj.id}" class="hidden">
             <input type="checkbox" id="${obj.id}"/>
-            <span id="span${obj.id}" class="inactive onDblClick">${obj.name}</span></li>
+            <span id="span${obj.id}" class="inactive onDblClick">${obj.name}</span>
+            <button class="hidden" id="button${obj.id}">delete</button></li>
             `));
 
             id++;
             $("#taskInput").val("");
             console.log(arr);
             itemsLeft();
+            btnCheck();
         };
-       
+        
     });
 
-    // TOGGLE SDELAN NAKONEC
-    let getter = document.getElementById('task');  
-
+    // TOGGLE SOLO CHECKBOXES, CLICK ON DEL BUTT
     getter.onclick = function(e) {
-        
+        // debugger
         document.getElementById.innerHTML = e.target.id;
         console.log(e.target.id);
-
+ 
         arr.forEach(obj => {
             // debugger
             if (obj.id === +e.target.id) { 
@@ -61,11 +72,29 @@ $(function() {
 
             };  
 
+            if ("button"+obj.id === e.target.id) {
+                // debugger
+                obj.status = "deleted";
+
+                let neededInd = arr.findIndex(obj => obj.status === "deleted");
+                    $("#list"+obj.id).remove();
+                    arr.splice(neededInd, 1)
+    
+            };  
+
         });
-        itemsLeft();
+        itemsLeft();   
+        btnCheck();
     };
 
-    $("#mainMarker").click(function() {
+    //ENABLE-DISABLE MARKER
+    function btnCheck() {
+        if (arr.length === 0) {btn.prop("disabled", true)} else {btn.prop("disabled", false)};
+    };
+    btnCheck();
+    
+    //MARKER
+    btn.click(function() {
 
         arr.forEach(obj => {
 
@@ -73,31 +102,32 @@ $(function() {
                 $("#span"+obj.id).toggleClass("active").removeClass("inactive");
                 $("#"+obj.id).prop("checked", true);
                 obj.isChecked = true;
-            } else {
+            }
+            else if (($("#"+obj.id).prop("checked"))){
+                
+            }
+            
+            else {
                 $("#span"+obj.id).toggleClass("inactive").removeClass("active");
                 $("#"+obj.id).prop("checked", false);
                 obj.isChecked = false;
             } 
             
-            // if ($("input:checkbox").prop("checked")) {
+            // if($("input:checkbox").prop("checked")) {
             //     $("#span"+obj.id).toggleClass("inactive").removeClass("active");
             //     $("#"+obj.id).prop("checked", false);
             //     obj.isChecked = false;
-            // };
-            // if (!obj.isChecked) { 
-            //     obj.isChecked = true;
+            // } else {
             //     $("#span"+obj.id).toggleClass("active").removeClass("inactive");
             //     $("#"+obj.id).prop("checked", true);
-            // } else {
-            //     obj.isChecked = false;
-            //     $("#span"+obj.id).toggleClass("inactive").removeClass("active");
-            //     $("#"+obj.id).prop("checked", false);
+            //     obj.isChecked = true;
             // }
+
         });
         itemsLeft();
-
     });
-
+    
+    //FILTERS
     $(".main__filter_active").click(function() {
 
         arr.forEach(obj => {
@@ -109,7 +139,6 @@ $(function() {
         });
 
     });
-
     $(".main__filter_done").click(function() {
 
         arr.forEach(obj => {
@@ -120,7 +149,6 @@ $(function() {
             }
         });
     });
-
     $(".main__filter_all").click(function() {
 
         arr.forEach(obj => {
@@ -130,35 +158,75 @@ $(function() {
     });
 
     $(".main__cleaner").click(function() {
+        //let needToDelete = arr.filter(obj => obj.isChecked === true)
 
         arr.forEach(obj => {
-            
-            if(obj.isChecked) {
-                indexToDelete = arr.findIndex(function() {
-                    return obj.id;
-                });
-                arr.slice(indexToDelete, 1);
+            if(obj.isChecked){
+                obj.status = "deleted";
+                debugger
+
+                let neededInd = arr.findIndex(obj => obj.status === "deleted");
                 $("#list"+obj.id).remove();
-                
-            };
-        });
+                arr.splice(neededInd, 1)
+
+                // $("#list"+obj.id).remove();
+                // arr.splice(0, 1)
+            }
+           
+        })
+
+
+        // arr.forEach(obj => {
+        //     if (obj.isChecked) {
+        //         obj.status = "deleted";
+        //     }
+        //     let needToDelete = arr.filter(obj => obj.status == "deleted")
+        // });
+
+
+        // if ("button"+obj.id === e.target.id) {
+        //     // debugger
+        //     obj.status = "deleted";
+
+        //     let neededInd = arr.findIndex(obj => obj.status === "deleted");
+        //         $("#list"+obj.id).remove();
+        //         arr.splice(neededInd, 1)
+
+        // };  
+
+        // arr.forEach(obj => {
+            
+        //     if(obj.isChecked) {
+        //         debugger
+        //         let indexToDelete = arr.indexOf(obj)
+        //         if (indexToDelete != -1) {
+        //         console.log(indexToDelete);
+        //         arr.splice(indexToDelete, 1);
+        //         $("#list"+obj.id).remove();
+        //         }
+        //     };
+
+
+        // });
+        btnCheck();
     });
 
-  
+    //RENAME
     getter.ondblclick = (function(e) {
         document.getElementById.innerHTML = e.target.id;
         
         arr.forEach(obj => {
             
-            if ("span"+obj.id === e.target.id) { 
+            if ("span"+obj.id === e.target.id || "list"+obj.id === e.target.id) { 
                 
                 inputVal = obj.name
                 $("#rewrite"+obj.id).removeClass("hidden").val(inputVal);
-                $("#"+obj.id).toggleClass("hidden");
-                $("#span"+obj.id).toggleClass("hidden");
+                $("#"+obj.id).addClass("hidden");
+                $("#span"+obj.id).addClass("hidden");
+
 
                 $("#rewrite"+obj.id).keyup(function(event){
-                    if((event.keyCode == 13) && (obj.name != "")) {
+                    if((event.keyCode == 13) && (obj.name != "") || (event.keyCode == 27)) {
                         obj.name = $("#rewrite"+obj.id).val()
 
                         $("#span"+obj.id).text(obj.name)
@@ -168,12 +236,62 @@ $(function() {
                         $("#rewrite"+obj.id).addClass("hidden");
                         $("#"+obj.id).removeClass("hidden");
                         $("#span"+obj.id).removeClass("hidden");
+
+                        
+                        // getter.onclick(function(e) {
+                        //     document.getElementById.innerHTML = e.target.id;
+                        //     if (e.target.id != "rewrite"+obj.id) {
+                        //         $("#rewrite"+obj.id).addClass("hidden");
+                        //         $("#"+obj.id).removeClass("hidden");
+                        //         $("#span"+obj.id).removeClass("hidden");
+                        //     }
+                        // });
+
                     };
+
                 });
+
+                // $("#rewrite"+obj.id).keyup(function(event){
+                //     if((event.keyCode == 27) && (obj.name != "")) {
+
+                //         $("#rewrite"+obj.id).addClass("hidden");
+                //         $("#"+obj.id).removeClass("hidden");
+                //         $("#span"+obj.id).removeClass("hidden");
+                //     };
+                // });
 
             };
 
         });
+    });
+
+    //BUT DELETE
+    getter.onmouseover = (function(e) {
+
+        document.getElementById.innerHTML = e.target.id;
+
+        arr.forEach(obj => {
+
+            if ("list"+obj.id === e.target.id || "span"+obj.id === e.target.id || obj.id === +e.target.id || "button"+obj.id === e.target.id) { 
+
+                $("#button"+obj.id).removeClass("hidden");
+
+            }; 
+        });
+
+    });
+    getter.onmouseout = (function(e) {
+        document.getElementById.innerHTML = e.target.id;
+
+        arr.forEach(obj => {
+            
+            if ("list"+obj.id === e.target.id || "span"+obj.id === e.target.id || obj.id === +e.target.id || "button"+obj.id === e.target.id) { 
+
+                $("#button"+obj.id).addClass("hidden");
+               
+            }; 
+        });
+
     });
 
 }); 
